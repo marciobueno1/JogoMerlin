a963ad4715e4aca8f59d0e84de9462e066d3d55d
Júlia Saldanha <juliasvilar@gmail.com>
2025-05-24 11:12:54 -0300

    retirando a possibilidade de voltar no tempo e do poder reinicia o jogo

--- app/jogo1.jsx
+++ app/jogo1.jsx
@@ -1,8 +1,6 @@
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import React, {useState} from 'react';
-import { Board } from "../components/Board"
-
-
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Board } from '../components/Board';
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
@@ -17,28 +15,20 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
+  const resetGame = () => {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  };
 
   return (
     <View style={styles.container}>
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      <ScrollView style={styles.moves}>
-        {history.map((_, move) => {
-          const description = move ? `Go to move #${move}` : 'Go to game start';
-          return (
-            <TouchableOpacity key={move} onPress={() => jumpTo(move)} style={styles.moveButton}>
-              <Text style={styles.moveText}>{description}</Text>
-            </TouchableOpacity>
-          );
-        })}
-      </ScrollView>
+      <TouchableOpacity onPress={resetGame} style={styles.moveButton}>
+        <Text style={styles.moveText}>Reiniciar Jogo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -47,24 +37,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#f2afbc',
   },
-  status: {
-    fontSize: 24,
-    marginTop: 100,
-    marginBottom: 20,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  
-  moves: {
-    marginTop: 30,
-  },
   moveButton: {
     padding: 10,
     backgroundColor: '#f1f1f1',
-    marginVertical: 5,
+    marginTop: 30,
     borderRadius: 5,
   },
   moveText: {
     fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
