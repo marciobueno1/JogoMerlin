aaa897ab9ee3be99631f602581f771f7a38d89a9
Marcio Bueno <marciobueno@gmail.com>
2025-05-24 10:08:48 -0300

    adicionando alguns styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,12 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Inicio" }} />
-      <Stack.Screen name="jogo1" options={{ title: "Jogo 1" }} />
-      <Stack.Screen name="jogo2" options={{ title: "Jogo 2" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Inicio", headerShown: false }}
+      />
+      <Stack.Screen name="jogo1" options={{ title: "Jogo da Velha" }} />
+      <Stack.Screen name="jogo2" options={{ title: "Quadrado Mágico" }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,24 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Lista de Jogos</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Jogo Merlin</Text>
       <Link href="/jogo1">Jogo 1</Link>
       <Link href="/jogo2">Jogo 2</Link>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+});
